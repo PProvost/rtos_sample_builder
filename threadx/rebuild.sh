e5d3c232e37f5577c84f5720186e755060a5516d
PProvost <peter@provost.org>
2020-03-27 15:30:04 -0600

    Cleanup

--- threadx/rebuild.sh
+++ threadx/rebuild.sh
@@ -6,8 +6,13 @@ if [ "$?" -ne "0" ]; then
     echo "Error creating build directory"
 fi
 
+# Clean up the build folder
 rm -rf *
+
+# Run cmake in the project root directory, using the toolchain
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/arm-gcc-toolchain.cmake ..
+
+# Run make to finish things off and generate our .bin file
 make
 
 popd
