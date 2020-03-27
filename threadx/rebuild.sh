b9727d14de6d04abf96d2380e25a9e6eb920cbc7
PProvost <peter@provost.org>
2020-03-27 15:29:01 -0600

    Cleanup

--- threadx/rebuild.sh
+++ threadx/rebuild.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+mkdir -p "build" # create if it doesn't exist
 pushd build
+if [ "$?" -ne "0" ]; then
+    echo "Error creating build directory"
+fi
 
 rm -rf *
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/arm-gcc-toolchain.cmake ..
