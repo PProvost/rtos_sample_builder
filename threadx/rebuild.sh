868bc53a8d67a9a5ec97cdfa9263422382d47870
PProvost <peter@provost.org>
2020-04-09 13:58:20 -0600

    Added vector table to sample

--- threadx/rebuild.sh
+++ threadx/rebuild.sh
@@ -11,8 +11,10 @@ BUILDDIR="$BASEDIR/build"
 mkdir -p $BUILDDIR
 rm -rf $BUILDDIR/*
 
-# Generate the build system
-cmake -B"$BUILDDIR" -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/arm-gcc-toolchain.cmake $BASEDIR
+# Generate the build system using Ninja
+cmake -B"$BUILDDIR" -GNinja -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/arm-gcc-toolchain.cmake $BASEDIR
+# Generate the build system using the system default
+# cmake -B"$BUILDDIR" -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/arm-gcc-toolchain.cmake $BASEDIR
 
 # And then do the build
 cmake --build $BUILDDIR
