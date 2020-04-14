c8b70a92c16b5e12a522853d2997b0ff024e6cec
PProvost <peter@provost.org>
2020-04-14 13:46:57 -0600

    Updates

--- peter_sample/cmake/cortex-m4.cmake
+++ peter_sample/cmake/cortex-m4.cmake
@@ -10,4 +10,4 @@ set(VFP_FLAGS "")
 set(SPEC_FLAGS "--specs=nosys.specs")
 # set(LD_FLAGS "-nostartfiles")
 
-include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-toolchain.cmake)
