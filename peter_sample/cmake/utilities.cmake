d2561f3042411164f139cd49a378ec15c426988d
PProvost <peter@provost.org>
2020-04-15 19:27:18 -0600

    Refactored cmake a bit

---
+++ peter_sample/cmake/utilities.cmake
@@ -0,0 +1,7 @@
+function(add_azrtos_component dirname targetname)
+    set(tmp ${azrtos_targets})
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/lib/${dirname})
+    target_link_options(${targetname} INTERFACE -T ${LINKER_SCRIPT})
+    list(APPEND tmp ${targetname})
+    set(azrtos_targets ${tmp} PARENT_SCOPE)
+endfunction()
