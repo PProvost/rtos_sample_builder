c815052a30558730a2fcc59a290fd89ab124201f
PProvost <peter@provost.org>
2020-04-15 19:30:56 -0600

    More refactoring

--- peter_sample/cmake/utilities.cmake
+++ peter_sample/cmake/utilities.cmake
@@ -1,6 +1,6 @@
 # TODO - this should take variable arguments so the user only has
 #        to provide the targetname if they need to
-function(add_azrtos_component dirname targetname)
+function(add_azrtos_component_dir dirname targetname)
     set(tmp ${azrtos_targets})
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/lib/${dirname})
     target_link_options(${targetname} INTERFACE -T ${LINKER_SCRIPT})
