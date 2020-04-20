c872eeeaf85863d4927508a8aab7cfb046062aeb
PProvost <peter@provost.org>
2020-04-20 12:22:51 -0600

    Cleanup

--- peter_sample/app/inc/gx_user.h
+++ peter_sample/app/inc/gx_user.h
@@ -1,1 +1,6 @@
+#ifndef NX_USER_H
+#define NX_USER_H
+
 // Nothing to see here
+
+#endif // NX_USER_H
--- peter_sample/app/inc/tx_user.h
+++ peter_sample/app/inc/tx_user.h
@@ -1,9 +1,9 @@
 #ifndef TX_USER_H
 #define TX_USER_H
 
-#ifndef TX_MAX_PRIORITIES
-#define TX_MAX_PRIORITIES                       32  
-#endif
+// #ifndef TX_MAX_PRIORITIES
+// #define TX_MAX_PRIORITIES                       32  
+// #endif
 
 #endif
 
