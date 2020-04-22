3e7c3a2b885e540d934e2a1bb4f0a6fc61f0d833
PProvost <peter@provost.org>
2020-04-22 12:46:25 -0600

    A little more script cleanup

--- threadx_simple/rebuild.sh
+++ threadx_simple/rebuild.sh
@@ -13,24 +13,23 @@
 # Use paths relative to this script's location
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
+BUILDDIR="$BASEDIR/build"
 
-if [ -z "$1" ]; then
-    TARGET_MCU="cortex_m4"
-else
-    TARGET_MCU=$1
-fi
+[ -z "$1" ] && TARGET_MCU="cortex_m4" || TARGET_MCU=$1
+TOOLCHAIN_FILE=$BASEDIR/cmake/${TARGET_MCU}.cmake
+
+echo "Using CMake toolchain ${TOOLCHAIN_FILE}"
 
 # If you want to build into a different directory, change this variable
-BUILDDIR="$BASEDIR/build"
 
 # Create our build folder if required and clear it
 mkdir -p $BUILDDIR
 rm -rf $BUILDDIR/*
 
 # Generate the build system using Ninja
-cmake -B"$BUILDDIR" -GNinja -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/${TARGET_MCU}.cmake $BASEDIR
+cmake -B"$BUILDDIR" -GNinja -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE $BASEDIR
 # Generate the build system using the system default
-# cmake -B"$BUILDDIR" -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/${TARGET_MCU}.cmake $BASEDIR
+# cmake -B"$BUILDDIR" -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE $BASEDIR
 
 # And then do the build
 cmake --build $BUILDDIR
