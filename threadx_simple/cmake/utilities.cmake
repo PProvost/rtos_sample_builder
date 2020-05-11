7a54d512e8f17c59e43ff4bb763f41e62b2f2629
PProvost <peter@provost.org>
2020-05-11 10:20:24 -0600

    Updated to v6.0_rc dependencies

--- threadx_simple/cmake/utilities.cmake
+++ threadx_simple/cmake/utilities.cmake
@@ -1,11 +1,15 @@
-# TODO - this should take variable arguments so the user only has
-#        to provide the targetname if they need to
-function(add_azrtos_component_dir dirname targetname)
+# Add the required subdirectory and register it for linkage
+function(add_azrtos_component_dir dirname)
+    # Store the current list in a temp
     set(tmp ${azrtos_targets})
+    # Add the subdir
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/lib/${dirname})
+    # If there is a linker script defined, use it
     if(EXISTS ${LINKER_SCRIPT})
-        target_link_options(${targetname} INTERFACE -T ${LINKER_SCRIPT})
+        target_link_options(${dirname} INTERFACE -T ${LINKER_SCRIPT})
     endif()
-    list(APPEND tmp "azrtos::${targetname}")
+    # Add this target into the temp
+    list(APPEND tmp "azrtos::${dirname}")
+    # Copy the temp back up to the parent list
     set(azrtos_targets ${tmp} PARENT_SCOPE)
 endfunction()
